7c94ffe5c225600a6b6be1f3420dc5e2b6f60dc8
Josué González <vortexwebdesign@gmail.com>
2013-02-12 11:40:41 -0800

    Act. 12/FEB/13
    Se agrega en carpeta admin página de login

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>
+Bienvenido a FamilyMail
+</title>
+</head>
+<body>
+<form action="#" method="post">
+	Usuario: <input name="username" id="username" maxlength="16"><br />
+	Password: <input type="password" name="password" id="password" maxlength="16"><br />
+	Email: <input name="email" id="email" maxlength="25"><br />
+	<input type="submit">
+</form>
+</body>
+</html>
